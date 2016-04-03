1383f9b8023d185907f189f95655a5cb45a0d07c
KimTaeyoun <intended@naver.com>
2016-04-04 03:02:06 +0900

    Update project1.py
    나이브 베이즈를 이용한 단어들의 긍정/부정 판단 프로젝트의 시작(참고자료부터)

--- project1.py
+++ project1.py
@@ -1,3 +1,15 @@
-# 
+# This is the very beginning of this project.
+# The goal of this project is to classify the set of words with probability theory, Naive Bayes.
+# I'm studying with the book written by Peter.
 
-# 플프롲프로젝프로젝틍프로젝트의
+from numpy import *
+
+def loadDataSet():
+  WordsList = [['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
+               ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
+               ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
+               ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
+               ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
+               ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
+  classVec = [0, 1, 0, 1, 0, 1]    # 1 is abusive, 0 not
+  return WordsList, classVec
