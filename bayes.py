954c48d6dd6a65d884ca12216a981f16f94c4cc0
KimTaeyoun <intended@naver.com>
2016-04-04 15:58:36 +0900

    Added files via upload

--- bayes.py
+++ bayes.py
@@ -5,6 +5,7 @@
 
 from numpy import *
 
+# 이 함수는 데이터를 읽어들인 후, 
 def loadDataSet():
   WordsList = [['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],   # This is an example. I can change these words to others.
                ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
@@ -18,17 +19,31 @@ def loadDataSet():
 def createVocabList(dataSet):    # When input a dataset.
   vocabSet = set([])       # Create an empty set
   for document in dataSet:
-    vocabSet = vacabSet | set(document)   # Create the union of two sets
+    vocabSet = vocabSet | set(document)   # Create the union of two sets
   return list(vocabSet)
   
 def setOfWords2Vec(vocabList, inputSet):    # When you import this fuction, setOfWords2Vec, with two input variables.
   returnVec = [0]*len(vocabList)    # Create a vector of all 0s
   for word in inputSet:
     if word in vocabList:
-      returnVecv[vocabList.index(word)] = 1
-      else: print "the word: %s is not in my Vocabulary!" % word
-      return returnVec
-      
-  
-  
-  
+      returnVec[vocabList.index(word)] = 1
+    else: print "the word: %s is not in my Vocabulary!" % word
+    return returnVec
+
+def trainNB0(trainMatrix, trainCategory):
+  numTrainDocs = len(trainMatrix)    # Number of the training documents
+  numWords = len(trainMatrix[0])    # Number of the words
+  pAbusive = sum(trainCategory) / float(numTrainDocs)  #
+  p0Num = zeros(numWords); p1Num = zeros(numWords)
+  p0Denom = 0.0; p1Denom = 0.0
+  for i in range(numTrainDocs):
+    if trainCategory[i] == 1:
+      p1Num += trainMatrix[i]
+      p1Denom += sum(trainMatrix[i])
+    else:
+      p0Num += trainMatrix[i]
+      p0Denom += sum(trainMatrix[i])
+  p1Vect = p1Num / p1Denom    #change to log()
+  p0Vect = p0Num / p0Denom    #change to log()
+  return p0Vect, p1Vect, pAbusive
+
